19f7e2c0c22507b1cb35cd638367a783d2749bbc
blake <opo2000tw@mail.fcu.edu.tw>
2021-05-19 09:13:43 +0800

    VSCode setting and comment

--- mmwave_sdk_68xx_hcc_mss/mss/hc_config_defs.h
+++ mmwave_sdk_68xx_hcc_mss/mss/hc_config_defs.h
@@ -18,42 +18,74 @@
 // #define PROFILE_2d
 // #endif
 
-
+// <- Cfg_ChannelCfgInitParams
+// LINK mrr_18xx_dss\common\cfg.c:375
 #define CHANNEL_HCC_CASCADING               (0U)
+// -> mmwDemo_sensorConfig_task::(Initialize the channel configuration)
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:982
 
+// <- Cfg_ADCOutCfgInitParams
+// LINK mrr_18xx_dss/common/cfg.c:400
 #define ADC_HCC_NUM_ADC_BITS                (2U)
 #define ADC_HCC_OUTPUT_FMT                  (1U)
+// -> mmwDemo_sensorConfig_task::(Initialize the ADCOut configuration)
+// LINK mmwave_sdk_68xx_hcc_mss\mmw_cli.c:976
 
+// <- Cfg_LowPowerModeInitParams
+// LINK mrr_18xx_dss/common/cfg.c:353
+#define LP_HCC_DONT_CARE                    (0U)
+#define LP_HCC_LOW_POWER_MODE               (0U)
+// ->  mmwDemo_sensorConfig_task::(Initialize the low power mode configuration)
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:985
+
+// <- MRR_DSS_dssDataPathConfigAdcBuf
+// LINK mrr_18xx_dss/dss_main.c:1093
 #define ADCBUF_HCC_SUBFRAME_IDX             (-1)
 #define ADCBUF_HCC_OUTPUT_FMT               (0U)
 #define ADCBUF_HCC_SAMPLE_SWAP              (1U)
 #define ADCBUF_HCC_CHAN_INTERLEAVE          (1U)
 #define ADCBUF_HCC_CHIRP_THRESHOLD          (1U)
+// -> MmwDemo_HCCADCBufCfg
+// LINK mmwave_sdk_68xx_hcc_mss\mmw_cli.c:646
 
-#define LP_HCC_DONT_CARE                    (0U)
-#define LP_HCC_LOW_POWER_MODE               (0U)
-
-#define CLUTTER_HCC_SUBFRAME_IDX            (-1)
+// ??? 
+#define CLUTTER_HCC_SUBFRAME_IDX            (-1) 
 #define CLUTTER_HCC_ENABLED                 (0)
+// -> MmwDemo_HCCClutterRemoval
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:619
 
+// ???
 #define AOAFOV_HCC_SUBFRAME_IDX             (-1)
 #define AOAFOV_HCC_MIN_AZIMUTH_DEG          (-90)
 #define AOAFOV_HCC_MAX_AZIMUTH_DEG          (90U)
 #define AOAFOV_HCC_MIN_ELEVATION_DEG        (-90)
 #define AOAFOV_HCC_MAX_ELEVATION_DEG        (90U)
+// -> MmwDemo_HCCAoAFovCfg
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:414
 
+// ???
 #define COMPRANGEBIASANDRXCHANPHASE         (0.0f)
+// -> MmwDemo_HCCCompRangeBiasAndRxChanPhaseCfg
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:675
 
+// ???
 #define EXTENDMAXVELOCITY_HCC_SUBFRAME_IDX  (-1)
 #define EXTENDMAXVELOCITY_HCC_ENABLED       (0U)
+// -> MmwDemo_HCCExtendedMaxVelocity
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:443
 
+// ???
 #define ANALOGMONITOR_HCC_RX_SATURATION     (0U)
 #define ANALOGMONITOR_HCC_SIG_IMG_BAND      (0U)
+// - >MmwDemo_HCCAnalogMonitorCfg
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:903
 
+// ???
 #define LVDSSTREAM_HCC_SUBFRAME_IDX         (-1)
 #define LVDSSTREAM_HCC_ENABLE_HEADER        (0U)
 #define LVDSSTREAM_HCC_DATA_FMT             (0U)
 #define LVDSSTREAM_HCC_ENABLE_SW            (0U)
+// -> MmwDemo_HCCLvdsStreamCfg
 
 #if defined(PROFILE_CALIBRATION)
     #include "../mss/hc_config_profile_calibration.h"
--- mmwave_sdk_68xx_hcc_mss/mss/hc_config_profile_3d.h
+++ mmwave_sdk_68xx_hcc_mss/mss/hc_config_profile_3d.h
@@ -40,6 +40,9 @@
 sensorStop
 flushCfg
 dfeDataOutputMode 1
+// DFE_DATA_OUTPUT_MODE_HCC
+// LINK mrr_18xx_mss/mss_mrr_cli.c:100
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:1212
 channelCfg 15 7 0
 adcCfg 2 1
 adcbufCfg -1 0 1 1 1
@@ -74,21 +77,27 @@ sensorStart
 
 */
 
-#define PROFILE_HCC_0_PROFILE_ID            (0U)
-#define PROFILE_HCC_0_START_FREQ_GHZ        (60U)
-#define PROFILE_HCC_0_IDLE_TIME_VAL         (7U)
+// <- Cfg_ProfileCfgInitParams
+// LINK mrr_18xx_dss/common/cfg.c:206
+#define PROFILE_HCC_0_ADC_SAMPLE_VAL        (256U)
 #define PROFILE_HCC_0_ADC_START_TIME_VAL    (3U)
+#define PROFILE_HCC_0_DIGOUT_SAMPLERATE_VAL (12500U)
+#define PROFILE_HCC_0_FREQ_SLOPE_MHZ_PER_US (166U)
+#define PROFILE_HCC_0_HPFCORNER_FREQ1_VAL   RL_RX_HPF1_175_KHz
+#define PROFILE_HCC_0_HPFCORNER_FREQ2_VAL   RL_RX_HPF2_350_KHz
+#define PROFILE_HCC_0_IDLE_TIME_VAL         (7U)
+#define PROFILE_HCC_0_PROFILE_ID            (0U)
 #define PROFILE_HCC_0_RAMP_END_TIME_VAL     (24U)
+#define PROFILE_HCC_0_RX_GAIN_VAL           (30U)
+#define PROFILE_HCC_0_START_FREQ_GHZ        (60U)
+#define PROFILE_HCC_0_TX_START_TIME_VAL     (1U)
 #define PROFILE_HCC_0_TXOUT_POWER_BACKOFF   (0U)
 #define PROFILE_HCC_0_TXPHASESHIFTER_VAL    (0U)
-#define PROFILE_HCC_0_FREQ_SLOPE_MHZ_PER_US (166U)
-#define PROFILE_HCC_0_TX_START_TIME_VAL     (1U)
-#define PROFILE_HCC_0_ADC_SAMPLE_VAL        (256U)
-#define PROFILE_HCC_0_DIGOUT_SAMPLERATE_VAL (12500U)
-#define PROFILE_HCC_0_HPFCORNER_FREQ1_VAL   (0U)
-#define PROFILE_HCC_0_HPFCORNER_FREQ2_VAL   (0U)
-#define PROFILE_HCC_0_RX_GAIN_VAL           (30U)
+// -> mmwDemo_sensorConfig_task::(Initializing and Adding Profiles)
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:998
 
+// <- Cfg_ChirpCfgInitParams
+// LINK mrr_18xx_dss\common\cfg.c:264
 #define CHIRP_HCC_0_START_INDEX             (0U)
 #define CHIRP_HCC_0_END_INDEX               (0U)
 #define CHIRP_HCC_0_PROFILE_ID              (0U)
@@ -97,7 +106,6 @@ sensorStart
 #define CHIRP_HCC_0_IDLE_TIME_VAL           (0U)
 #define CHIRP_HCC_0_ADC_START_TIME_VAL      (0U)
 #define CHIRP_HCC_0_TX_CHANNEL              (1U)
-
 #define CHIRP_HCC_1_START_INDEX             (1U)
 #define CHIRP_HCC_1_END_INDEX               (1U)
 #define CHIRP_HCC_1_PROFILE_ID              (0U)
@@ -106,7 +114,6 @@ sensorStart
 #define CHIRP_HCC_1_IDLE_TIME_VAL           (0U)
 #define CHIRP_HCC_1_ADC_START_TIME_VAL      (0U)
 #define CHIRP_HCC_1_TX_CHANNEL              (4U)
-
 #define CHIRP_HCC_2_START_INDEX             (2U)
 #define CHIRP_HCC_2_END_INDEX               (2U)
 #define CHIRP_HCC_2_PROFILE_ID              (0U)
@@ -115,12 +122,24 @@ sensorStart
 #define CHIRP_HCC_2_IDLE_TIME_VAL           (0U)
 #define CHIRP_HCC_2_ADC_START_TIME_VAL      (0U)
 #define CHIRP_HCC_2_TX_CHANNEL              (2U)
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:1107
+// -> mmwDemo_sensorConfig_task::(Initializing and Adding Chirps)
 
+// <- MRR_MSS_CLISensorStart::calibrationCfg.dfeDataOutputMode
+// LINK mrr_18xx_mss/mss_mrr_cli.c:118
 #define DFE_DATA_OUTPUT_MODE_HCC            (1U)
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:1212
 
+// <- Cfg_ChannelCfgInitParams
+// LINK mrr_18xx_dss\common\cfg.c:375
 #define CHANNEL_HCC_RX_CHANNEL_EN           (15U)
 #define CHANNEL_HCC_TX_CHANNEL_EN           (7U)
+// LINK mmwave_sdk_68xx_hcc_mss\mss\hc_config_defs.h:35
+// -> mmwDemo_sensorConfig_task::(Configuration :: Open Sensor)
 
+/* SUBFRAME Configuration */
+// <- Cfg_FrameCfgInitParams
+// LINK mrr_18xx_dss/common/cfg.c:175
 #define FRAME_HCC_CHIRP_START_INDEX         (0U)
 #define FRAME_HCC_CHIRP_END_INDEX           (2U)
 #define FRAME_HCC_NUM_LOOPS                 (32U)
@@ -128,16 +147,30 @@ sensorStart
 #define FRAME_HCC_FRAME_PERIODICITY         (100U)
 #define FRAME_HCC_TRIGGER_SELECT            (1U)
 #define FRAME_HCC_FRAME_TRIG_DELAY          (0U)
+// -> mmwDemo_sensorConfig_task::(Initializing Output Mode and Frame Configurations) 
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:1219
 
-#define GUI_HCC_0_SUBFRAME_IDX              (-1)
+
+/* 
+ * void MmwDemo_populateUSRR(MmwDemo_DSS_DataPathObj* obj, uint16_t subframeIndx) 
+ * // LINK mrr_18xx_dss/dss_main.c:659
+ * // LINK mrr_18xx_dss/dss_main.c:1511
+ * XXX_SUBFRAME_IDX = uint16_t subframeIndx
+ */
+#pragma region subframe
+// ???
+#define GUI_HCC_0_SUBFRAME_IDX              (-1) // ???
 #define GUI_HCC_0_DETECTED_OBJECTS          (1U)
 #define GUI_HCC_0_LOG_MAGNITUDE_RANGE       (1U)
 #define GUI_HCC_0_NOISE_PROFILE             (1U)
 #define GUI_HCC_0_RANGE_AZIMUTH_MAP         (0U)
 #define GUI_HCC_0_RANGE_DOPPLER_MAP         (0U)
 #define GUI_HCC_0_STATS_INFO                (1U)
+// -> MmwDemo_HCCGuiMonSel
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:121
 
-#define CFAR_HCC_0_SUBFRAME_IDX             (-1)
+// ???
+#define CFAR_HCC_0_SUBFRAME_IDX             (-1) // ???
 #define CFAR_HCC_0_PROC_DIRECTION           (0U)
 #define CFAR_HCC_0_MODE                     (2U)
 #define CFAR_HCC_0_NOISE_WIN                (8U)
@@ -146,7 +179,6 @@ sensorStart
 #define CFAR_HCC_0_CYCLIC_MODE              (0U)
 #define CFAR_HCC_0_THRESHOLD_SCALE          (15.0f)
 #define CFAR_HCC_0_PEAK_GROUPING            (0U)
-
 #define CFAR_HCC_1_SUBFRAME_IDX             (-1)
 #define CFAR_HCC_1_PROC_DIRECTION           (1U)
 #define CFAR_HCC_1_MODE                     (0U)
@@ -156,42 +188,66 @@ sensorStart
 #define CFAR_HCC_1_CYCLIC_MODE              (1U)
 #define CFAR_HCC_1_THRESHOLD_SCALE          (15.0f)
 #define CFAR_HCC_1_PEAK_GROUPING            (0U)
+// -> MmwDemo_HCCCfarCfg
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:194
 
-#define MULTI_OBJ_BEAM_HCC_0_SUBFRAME_IDX   (-1)
+// ???
+#define MULTI_OBJ_BEAM_HCC_0_SUBFRAME_IDX   (-1) // ???
 #define MULTI_OBJ_BEAM_HCC_0_ENABLED        (1U)
 #define MULTI_OBJ_BEAM_HCC_0_THRESHOLD      (0.5f)
+// -> MmwDemo_HCCMultiObjBeamForming
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:474
 
-#define CALIB_DC_RANGE_HCC_0_SUBFRAME_IDX   (-1)
+// ???
+#define CALIB_DC_RANGE_HCC_0_SUBFRAME_IDX   (-1) /// ???
 #define CALIB_DC_RANGE_HCC_0_ENABLED        (0U)
 #define CALIB_DC_RANGE_HCC_0_NEG_BIN_IDX    (-5)
 #define CALIB_DC_RANGE_HCC_0_POS_BIN_IDX    (8U)
 #define CALIB_DC_RANGE_HCC_0_NUM_AVG        (256U)
+// -> MmwDemo_HCCCalibDcRangeSig
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:532
 
-#define CFAR_FOV_HCC_0_SUBFRAME_IDX         (-1)
+// ???
+#define CFAR_FOV_HCC_0_SUBFRAME_IDX         (-1) /// ???
 #define CFAR_FOV_HCC_0_PROC_DIRECTION       (0U)
 #define CFAR_FOV_HCC_0_MINIMUM              (0.25f)
 #define CFAR_FOV_HCC_0_MAXIMUM              (9.0f)
-
 #define CFAR_FOV_HCC_1_SUBFRAME_IDX         (-1)
 #define CFAR_FOV_HCC_1_PROC_DIRECTION       (1U)
 #define CFAR_FOV_HCC_1_MINIMUM              (-20.16f)
 #define CFAR_FOV_HCC_1_MAXIMUM              (20.16f)
+// -> MmwDemo_HCCCfarFovCfg
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:325
 
+// ???
+#define BPM_HCC_SUBFRAME_IDX                (-1)
+#define BPM_HCC_ENABLED                     (0U)
+#define BPM_HCC_CHIRP_0_IDX                 (0U)
+#define BPM_HCC_CHIRP_1_IDX                 (0U)
+// -> MmwDemo_HCCBpmCfg
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:757
+#pragma endregion subframe
+
+// ???
 #define MEASURERANGEBIAS_HCC_ENABLED        (0U)
 #define MEASURERANGEBIAS_HCC_TARGET_DIST    (1.0f)
 #define MEASURERANGEBIAS_HCC_SEARCH_WIN     (0.2f)
+// -> MmwDemo_HCCMeasureRangeBiasAndRxChanPhaseCfg
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:722
 
+// ???
 #define CQRXSAT_HCC_0_PROFILE_ID            (0U)
 #define CQRXSAT_HCC_0_SAT_MON_SEL           (3U)
 #define CQRXSAT_HCC_0_PRI_SLICE_DURATION    (4U)
 #define CQRXSAT_HCC_0_NUM_SLICES            (63U)
 #define CQRXSAT_HCC_0_RX_CHAN_MASK          (0U)
+// -> MmwDemo_HCCChirpQualityRxSatMonCfg
+// mmwave_sdk_68xx_hcc_mss/mmw_cli.c:790
 
+// ???
 #define CQSIGIMG_HCC_0_PROFILE_ID           (0U)
 #define CQSIGIMG_HCC_0_NUM_SLICES           (127U)
 #define CQSIGIMG_HCC_0_NUM_SAMPLE_PER_SLICE (4U)
+// -> MmwDemo_HCCChirpQualitySigImgMonCfg
+// LINK mmwave_sdk_68xx_hcc_mss/mmw_cli.c:852
 
-#define BPM_HCC_SUBFRAME_IDX                (-1)
-#define BPM_HCC_ENABLED                     (0U)
-#define BPM_HCC_CHIRP_0_IDX                 (0U)
-#define BPM_HCC_CHIRP_1_IDX                 (0U)
